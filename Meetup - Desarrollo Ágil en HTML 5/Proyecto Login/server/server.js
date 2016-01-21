bd6c1f1b4c72ff76480b50ef9117961135a29fe2
badillosoft <badillo.soft@hotmail.com>
2016-01-21 02:56:07 -0600

    Se implemento la base de datos mongo mediante mongoose.
    Se agregarón los códigos de error correspondientes a los generados por la base de datos.

--- Meetup - Desarrollo Ágil en HTML 5/Proyecto Login/server/server.js
+++ Meetup - Desarrollo Ágil en HTML 5/Proyecto Login/server/server.js
@@ -3,22 +3,86 @@
 var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var app = express();
+
+mongoose.connect('mongodb://localhost/LoginProject');
+
+var db = mongoose.connection;
+
+var connected = false;
+
+db.on('error', function (err) {
+	console.log('No se puede establecer conexión a la base de datos');
+});
+
+var UserSchema = mongoose.Schema({ username: String, password: String });
+
+var Users = mongoose.model('Users', UserSchema);
+
+db.once('open', function () {
+	console.log('Conectado a la base de datos');
+	connected = true;
+	
+	var john = new Users({ username: 'john', password: 'doe' });
+	
+	john.save();
+}); 
 
 app.use(bodyParser.json());
 
 app.post('/login', function(req, res) {
-	var data = req.body;
-	
-	console.log(data);
-	
-	if (data.username === 'john' && data.password === 'doe') {
-		res.send(JSON.stringify({code: 0, token: "ABC123"}));
+	if (!connected) {
+		res.send(JSON.stringify({
+			code: 100,
+			message: "Error al conectarse a la base de datos"
+		}));
 		return;
 	}
 	
-	res.send(JSON.stringify({code: 1, message: "Datos de acceso incorrectos"}));
+	var data = req.body;
+	
+	console.log('Validando usuario: ' + JSON.stringify(data));
+	
+	if (!data.username) {
+		res.send(JSON.stringify({
+			code: 2,
+			message: "No se ha recibido el nombre de usuario"
+		}));
+		return;
+	}
+	
+	if (!data.password) {
+		res.send(JSON.stringify({
+			code: 3,
+			message: "No se ha recibido la contraseña"
+		}));
+		return;
+	}
+	
+	Users.findOne(
+		{username: data.username, password: data.password},
+		function (err, user) {
+			if (err) {
+				res.send(JSON.stringify({
+					code: 101,
+					message: "Falló la consulta a la base de datos"
+				}));
+				return;
+			}
+			
+			if (!user) {
+				res.send(JSON.stringify({
+					code: 1, 
+					message: "Datos de acceso incorrectos"
+				}));
+				return;
+			}
+			
+			res.send(JSON.stringify({code: 0, token: "ABC123"}));
+		}
+	);
 });
 
 http.createServer(app).listen(8080, function () {
